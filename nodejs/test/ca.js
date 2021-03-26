c84d6a46e7ab3a8f98fd35dd0f57d3f514c126a1
DavidLiu <david.yx.liu@oracle.com>
2021-03-26 04:59:05 -0400

    enroll to existing IDCS account

--- nodejs/test/ca.js
+++ nodejs/test/ca.js
@@ -1,37 +1,62 @@
-const {register} = require('khala-fabric-sdk-node/ca')
 const CAService = require('khala-fabric-sdk-node-builder/ca')
-const {getAdmin_founder_Client} = require('./testUtil')
-const fs = require('fs')
 const AffiliationService = require('khala-fabric-sdk-node-builder/affiliationService');
 const IdentityService = require('khala-fabric-sdk-node-builder/identityService')
-
-describe('ca info list', function () {
+const {ECDSA_Key} = require('khala-fabric-formatter/key')
+const UserBuilder = require('khala-fabric-sdk-node-builder/user')
+const fs = require('fs')
+const path = require('path')
+const assert = require('assert')
+describe('ca', function () {
     this.timeout(30000)
     const caUrl = 'https://founder-4-hktwlab-iad.blockchain.ocp.oraclecloud.com:7443'
     const CAName = 'founderca'
     const {caService} = new CAService(caUrl, undefined, CAName)
-    const {_userContext: admin} = getAdmin_founder_Client();
+    const enrollmentID = process.env.IDCS_ID
+    const enrollmentSecret = process.env.IDCS_PASSWORD
+    const userBuilder = new UserBuilder({name:enrollmentID})
 
-
+    const keystore=path.resolve(`test/artifacts/founder-user-credential/${enrollmentID}-key`)
+    const signcert= path.resolve(`test/artifacts/founder-user-credential/${enrollmentID}-cert.pem`)
     it('enroll: required Oracle IDCS credential', async () => {
-        const enrollmentID = process.env.IDCS_ID
-        const enrollmentSecret = process.env.IDCS_PASSWORD
+
 
         const {certificate, rootCertificate, key} = await caService.enroll({enrollmentID, enrollmentSecret});
         console.info({certificate})
         console.info({rootCertificate})
-        console.debug(key)
+        const ecdsaKey = new ECDSA_Key(key, fs)
+
+        ecdsaKey.save(keystore)
+
+        fs.writeFileSync(signcert, certificate)
     })
-    it.skip('list affiliation', async () => {
+    it('list affiliation: Not supported', async () => {
+
+        const user = userBuilder.build({
+            key: fs.readFileSync(keystore),
+            certificate: fs.readFileSync(signcert),
+            mspId:'founder'
+        });
         const {affiliationService} = new AffiliationService(caService);
-        const result = await affiliationService.getAll(admin)
-        console.info(result)
+        try {
+            await affiliationService.getAll(user)
+        }catch (e){
+            // Failed to get affiliation: Not supported
+            assert.strictEqual(`fabric-ca request affiliations failed with errors [[{"code":49,"message":"Failed to get affiliation: Not supported"}]]`, e.message)
+        }
+
+
     })
-    it.skip('list id', async () => {
+    it('list id: Not supported', async () => {
+        const user = userBuilder.build({
+            key: fs.readFileSync(keystore),
+            certificate: fs.readFileSync(signcert),
+            mspId:'founder'
+        });
         const idService = new IdentityService(caService);
-        const allIDs = await idService.getAll(admin);
-        for (const id of allIDs) {
-            console.info(id.id, id.attrs);
+        try {
+            await idService.getAll(user);
+        }catch (e){
+            assert.strictEqual(`fabric-ca request identities?ca=founderca failed with errors [[{"code":49,"message":"Failed to get users by affiliation and type: Not supported"}]]`,e.message)
         }
     })
 })
