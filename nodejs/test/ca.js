f64f7e4d774377a6b263dcda5a3639a33e7e9172
davidliu <david-khala@hotmail.com>
2022-08-08 23:10:34 +0800

    [DONE] enroll: with CSR

--- nodejs/test/ca.js
+++ nodejs/test/ca.js
@@ -1,12 +1,20 @@
-const CAService = require('khala-fabric-sdk-node-builder/ca')
-const AffiliationService = require('khala-fabric-sdk-node-builder/affiliationService');
-const IdentityService = require('khala-fabric-sdk-node-builder/identityService')
-const CertificateService = require('khala-fabric-sdk-node-builder/certificateService')
-const {ECDSA_Key} = require('khala-fabric-formatter/key')
-const UserBuilder = require('khala-fabric-sdk-node-builder/user')
-const fs = require('fs')
-const path = require('path')
-const assert = require('assert')
+import CAService from 'khala-fabric-admin/ca.js'
+import AffiliationService from 'khala-fabric-sdk-node/affiliationService.js';
+import IdentityService from 'khala-fabric-sdk-node/identityService.js'
+import {ECDSA_PrvKey} from 'khala-fabric-formatter/key.js'
+import UserBuilder from 'khala-fabric-admin/user.js'
+import {ping} from 'khala-fabric-sdk-node/ca.js'
+import {Key, Vault} from '@davidkhala/oci-key-vault/index.js'
+import {FileAuthentication} from '@davidkhala/oci-common/index.js'
+import {ECDSAKey} from "@davidkhala/crypto/ECDSA.js";
+import X500Name from "@davidkhala/crypto/X500Name.js";
+import {Extension} from "@davidkhala/crypto/extension.js";
+import {CSR} from '@davidkhala/crypto/pkcs10.js'
+import fs from 'fs'
+import path from 'path'
+import assert from 'assert'
+
+const auth = new FileAuthentication()
 const enrollmentID = process.env.IDCS_ID
 if (!enrollmentID) {
     throw Error('process.env.IDCS_ID not found')
@@ -15,7 +23,7 @@ const enrollmentSecret = process.env.IDCS_PASSWORD
 if (!enrollmentSecret) {
     throw  Error('process.env.IDCS_PASSWORD not found')
 }
-const keystore = path.resolve(`test/artifacts/founder-user-credential/${enrollmentID}-key`)
+const keystore = path.resolve(`test/artifacts/founder-user-credential/priv_sk`)
 const signcert = path.resolve(`test/artifacts/founder-user-credential/${enrollmentID}-cert.pem`)
 const getUser = () => {
     const userBuilder = new UserBuilder({name: enrollmentID})
@@ -27,13 +35,15 @@ const getUser = () => {
 }
 describe('ca', function () {
     this.timeout(0)
-    const caUrl = 'https://founder-5-hktwlab-iad.blockchain.ocp.oraclecloud.com:7443'
-    const CAName = 'founderca'
-    const {caService} = new CAService(caUrl, undefined, CAName)
+    const protocol = 'https'
+    const hostname = 'founder-2-hktwlab-sin.blockchain.ocp.oraclecloud.com'
+    const port = 7443
+    const caUrl = `${protocol}://${hostname}:${port}/`
+    const caService = new CAService({protocol, port, hostname, timeout: 10000})
 
 
     it('http ping', async () => {
-        const {ping} = require('khala-fabric-sdk-node/ca')
+
         const result = await ping(caUrl)
         console.info(result)
         assert.strictEqual(result.Version, '1.4.4')
@@ -45,9 +55,45 @@ describe('ca', function () {
         const {certificate, rootCertificate, key} = await caService.enroll({enrollmentID, enrollmentSecret});
         console.info({certificate})
         console.info({rootCertificate})
-        const ecdsaKey = new ECDSA_Key(key, fs)
+        const ecdsaKey = new ECDSA_PrvKey(key)
 
-        ecdsaKey.save(keystore)
+        ecdsaKey.toKeystore(path.dirname(keystore))
+
+        fs.writeFileSync(signcert, certificate)
+    })
+    it('enroll: with CSR', async () => {
+        const vaultId = 'ocid1.vault.oc1.ap-singapore-1.enrhpwtoaabem.abzwsljrk57oclvejakgkh42rblwi7dmymmhnfrmt7nmloagt24mcrpl236q'
+        const vault = new Vault(auth)
+        const health = await auth.connect()
+        assert.ok(health)
+        const oneVault = await vault.get(vaultId)
+
+        const keyID = 'ocid1.key.oc1.ap-singapore-1.enrhpwtoaabem.abzwsljrizggn5jlznyv7j64ccchehu6wc6vmfllyobilups4ahhp34pzyqq'
+        const key = new Key(auth, oneVault)
+        const keyPEM = await key.publicKeyOf(keyID)
+
+        const publicKey = ECDSAKey.FromPEM(keyPEM);
+        const subject = new X500Name();
+        subject.setCountryName('HK');
+        subject.setOrganizationName('Oracle');
+        subject.setOrgUnitName('Cloud Platform');
+        subject.setCommonName(enrollmentID);
+        const extensions = Extension.asSAN(['*.hyperledger.org']);
+
+        const csr = new CSR({subject, pubKeyObj: publicKey.pubKeyObj, extensions});
+
+        const unsignedHex = csr.getUnsignedHex();
+        const message = Buffer.from(unsignedHex, 'hex')
+        const signature_base64 = await key.sign(keyID, message)
+        console.info(signature_base64);
+        const sig_hex = Buffer.from(signature_base64, 'base64').toString('hex')
+        const signatureAlgorithm = 'SHA256withECDSA'
+        const pem = csr.toPemWithSignature(sig_hex, signatureAlgorithm)
+        console.info(pem)
+
+        const {certificate, rootCertificate} = await caService.enroll({enrollmentID, enrollmentSecret, csr:pem});
+        console.info({certificate})
+        console.info({rootCertificate})
 
         fs.writeFileSync(signcert, certificate)
     })
@@ -74,14 +120,6 @@ describe('ca', function () {
         } catch (e) {
             assert.strictEqual(`fabric-ca request identities?ca=founderca failed with errors [[{"code":49,"message":"Failed to get users by affiliation and type: Not supported"}]]`, e.message)
         }
-    })
-    it('revoke user and it still works', async () => {
-        const user = getUser()
-        const certificate = fs.readFileSync(signcert, 'utf-8');
-        const certificateService = new CertificateService(caService)
-        const result = await certificateService.revokeCertificate(certificate, user)
-        console.debug(result)
-
     })
 })
 
